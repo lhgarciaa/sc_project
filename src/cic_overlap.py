ae693cd40f9201d19c954ed1b858676dbb2a52e0
Ian Bowman <ibowman@loni.usc.edu>
2017-06-15 18:51:31 -0700

    CIC overlap csv utility methods.

---
+++ src/cic_overlap.py
@@ -0,0 +1,44 @@
+#general utilities related to 
+import csv
+
+def read_overlap_csv(input_csv_path):
+    """Return an overlap csv 
+
+    Args:
+        input_csv_path
+    
+    Returns:
+       dict: dictionary with meta value key value pairs
+       list: list of header labels
+       list: list of rows in csv
+    """
+    meta_dct = {}
+    header_lst = []
+    rows = []
+
+    with open(input_csv_path, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile)
+        num_metalines = -1
+        for row_index, row in enumerate(csvreader):
+            if row_index == 0:
+                assert len(row) == 1
+                metalines_row_arr = row[0].split(':')
+                assert metalines_row_arr[0] == 'Metalines'
+                num_metalines = int(metalines_row_arr[1])
+
+            elif row_index < num_metalines:
+                assert len(row) == 1
+                row_arr = row[0].split(':')
+                meta_dct[row_arr[0].strip()] = row_arr[1].strip()
+                
+            elif row_index == num_metalines:
+                header_lst = [col.strip() for col in row]
+
+            else:
+                rows.append([col.strip() for col in row])
+
+    return (meta_dct, header_lst, rows)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
