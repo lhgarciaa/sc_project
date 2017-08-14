bf154f0c7259f2aa446e0f8dbfeba6572a388bea
Ian Bowman <ibowman@loni.usc.edu>
2017-08-14 16:44:45 -0700

    Shorter and better job names

--- src/ms_char_cmt_str.sh
+++ src/ms_char_cmt_str.sh
@@ -13,8 +13,9 @@ for file in $files
 do
     name_base=`basename $file` # for output file
     logs_dir=qsub_logs
-    log_name_base=$logs_dir/char_cmt_${name_base}
-    qsub -cwd -q compute.q -o $log_name_base.out -e $log_name_base.err -l hostslots=1 src/qsub_char_cmt_str.sh $file $venv_dir
+    log_name_base=$logs_dir/cc_${name_base}
+    job_name=cc-$name_base
+    qsub -N $job_name -cwd -q compute.q -o $log_name_base.out -e $log_name_base.err -l hostslots=1 src/qsub_char_cmt_str.sh $file $venv_dir
 done
 
 
