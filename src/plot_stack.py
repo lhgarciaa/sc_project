6a951df9aba2e086650b8d711009507d4212dc09
lgar <luis.garcia@loni.usc.edu>
2020-02-11 08:12:03 -0800

    Add plotting script for SC stacked bar charts and tables/matrices

---
+++ src/plot_stack.py
@@ -0,0 +1,316 @@
+from __future__ import print_function
+import matplotlib
+matplotlib.use('Agg')
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import os
+from cic_dis import cic_utils
+# import cPickle as pickle
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Creates stacked bar graphs using row/column"
+                    " connectity matrix")
+
+    parser.add_argument('-m', '--matrix_csv',
+                        help='Row/column based connectivity matrix',
+                        required=True)
+
+    parser.add_argument('-v', '--verbose',
+                        help='Print matrix, cluster related output',
+                        action='store_true')
+
+    parser.add_argument('-fmt', '--format',
+                        help='Format of plot (e.g. svg, png,...)',
+                        default='svg')
+
+    parser.add_argument('-fs', '--fontsize',
+                        help='Font size of tick labels in x and y',
+                        type=int,
+                        default=2)
+
+    parser.add_argument('-ct', '--chart_type',
+                        help='Type of chart to generate: single,'
+                        'levels, ',
+                        choices=['single', 'multi_prop', 'levels'],
+                        default='single')
+
+    parser.add_argument('-vt', '--value_type',
+                        help='Type of value being used, raw_pixel, proportion'
+                             'for y axis labeling',
+                        choices=['raw_pixel', 'proportion'])
+
+    parser.add_argument('-inj', '--injection_site',
+                        help='Injection site of interest to graph e.g., ACAv, '
+                             'RSPv, RSPd, can take in comma seperated list',
+                        default='all')
+
+    args = vars(parser.parse_args())
+
+    matrix_csv_path = args['matrix_csv']
+    assert os.path.isfile(matrix_csv_path),\
+        "Can't find row column matrix csv file {}".format(matrix_csv_path)
+
+    #PARSE ARGUMENTS
+    verbose = args['verbose']
+    fontsize = args['fontsize']
+    fmt = "{}".format(args['format'])
+    chart_type = args['chart_type']
+    value_type = args['value_type']
+    injection_site = args['injection_site']
+    colors = ('#990000', '#FF6600', '#116600', '#660078')
+    names = ('SC.m', 'SC.cm', 'SC.cl', 'SC.l')
+    output_dir, base_name = os.path.split(matrix_csv_path)
+    out_path = ''
+
+    if injection_site is not 'all':
+        site_lst = injection_site.split(',')
+
+    # Open, read input csv
+    (row_roi_name_npa, col_roi_name_npa, ctx_mat_npa) = \
+        cic_utils.read_ctx_mat(matrix_csv_path)
+
+    if verbose:
+        print("Read row ROIs: \n{}\n".format(row_roi_name_npa))
+        print("Read col ROIs: \n{}\n".format(col_roi_name_npa))
+
+    # If user provided injection_sites, assert that sites are valid
+    if injection_site is not 'all':
+        for site in site_lst:
+            pass
+            assert site in row_roi_name_npa, \
+                "{} is not in row roi list...\n{}".format(site,
+                                                          row_roi_name_npa)
+    else:
+        site_lst = row_roi_name_npa
+
+    roi_dict = {}
+
+    # for all the sites in site_lst, get values
+    for site in range(len(site_lst)):
+        # division level dictionary
+        div_level_dict = {}
+
+        site_idx = list(row_roi_name_npa).index(site_lst[site])
+        site_vals = ctx_mat_npa[site_idx]
+        # Place site for a specific inj site in dictionary
+        for idx, value in enumerate(site_vals):
+            div_level = col_roi_name_npa[idx]
+            div_level_dict[div_level] = value
+
+        graph_name = base_name.replace(
+            '.csv',
+            '_stacked_{}_{}.{}'.format(chart_type,
+                                       value_type,
+                                       fmt))
+
+        out_path = os.path.join(output_dir, graph_name)
+
+        if chart_type == 'levels':
+            generate_levels_chart(div_level_dict,
+                                  site_lst[site],
+                                  out_path,
+                                  value_type,
+                                  colors)
+        elif chart_type == 'multi_prop':
+            roi_values = get_div_proportional_values(div_level_dict)
+            roi_dict[site_lst[site]] = roi_values
+        else:
+            generate_single_chart(div_level_dict,
+                                  site_lst[site],
+                                  out_path,
+                                  colors)
+
+    if chart_type == 'multi_prop':
+        graph_name = base_name.replace(
+            '.csv',
+            '_stacked_{}_{}.{}'.format(chart_type,
+                                       value_type,
+                                       fmt))
+        out_path = os.path.join(output_dir, graph_name)
+        generate_proportion_group_chart(roi_dict, "you", out_path, names, colors)
+        print("These are the values: ")
+        print(roi_dict)
+
+    print("Done")
+
+
+# Generates single stacked bar chart with proportional labeling, aggregates
+# div values across all levels
+def generate_single_chart(div_lvl_dict, title, out_path, colors):
+    lvls = 1
+    ind = np.arange(lvls)
+    width = 0.25
+
+    div_values = np.zeros(4)
+
+    for site in div_lvl_dict:
+        if 'SC.m' in site:
+            div_values[0] += div_lvl_dict[site]
+        elif 'SC.cm' in site:
+            div_values[1] += div_lvl_dict[site]
+        elif 'SC.cl' in site:
+            div_values[2] += div_lvl_dict[site]
+        else:
+            div_values[3] += div_lvl_dict[site]
+
+    div_values = np.split(div_values, 4)
+    col_totals = np.sum(div_values, axis=0)
+    div_values = np.nan_to_num(div_values / col_totals) * 100
+
+    bars = []
+    bottoms = np.zeros(1)
+    for idx, division in enumerate(div_values):
+        bars.append(plt.bar(
+            ind, division, bottom=bottoms, color=colors[idx], width=width))
+        bottoms += division
+
+    plt.bar(ind, 0, 1.0, align='center', color='black', label=None)
+
+    plt.xticks(ind, (title,))
+    plt.legend((bars[0][0], bars[1][0], bars[2][0], bars[3][0]),
+               ('SC.m', 'SC.cm', 'SC.cl', 'SC.l'))
+
+    plt.savefig(out_path, dpi=600)
+    plt.close()
+
+
+def generate_proportion_group_chart(roi_dict, title, out_path, names, colors):
+    # labels = list(roi_dict.keys())
+    if 'asso' in out_path:
+        # labels = ["RSPv_interm", "RSPv_caudal", "RSPd_rostral", "RSPagl", "PTLp_lat", "RSPd_caudal", "ACAv_interm", "TEa_caudal", "ACAv_caudal", "PTLp_med", "ACAd_interm", "ACAv_rostral", "RSPv_rostral", "ACAd_rostral", "ACAd_caudal", "ORBm", "PL", "ILA", "TEa_rostral", "ORBvl", "ORBl"]
+        # labels = ["RSPv_interm", "RSPv_caudal", "RSPd_rostral", "RSPagl",
+        #           "RSPd_caudal", "RSPv_rostral", "PTLp_lat", "TEa_caudal",
+        #           "ACAv_interm", "ACAv_caudal", "ACAd_interm", "ACAv_rostral",
+        #           "PTLp_med", "ACAd_rostral", "ACAd_caudal", "TEa_rostral",
+        #           "ORBm", "PL", "ILA", "ORBvl", "ORBl"]
+        labels = ["RSPv_interm", "RSPv_caudal", "RSPd_rostral", "RSPagl", "RSPd_caudal", "PTLp_lat", "TEa_caudal", "ACAv_caudal", "ACAv_interm", "ACAv_rostral", "PTLp_med", "RSPv_rostral", "ACAd_interm", "ACAd_caudal", "ACAd_rostral", "TEa_rostral", "ORBm", "PL", "ILA", "ORBvl", "ORBl"]
+        # labels = ["RSPv_interm", "RSPv_caudal", "RSPd_rostral", "RSPagl", "RSPd_caudal", "PTLp_lat", "TEa_caudal", "ACAv_caudal", "ACAv_interm", "ACAv_rostral", "ACAd_interm", "ACAd_caudal", "ACAd_interm", "ACAd_rostral", "RSPv_rostral", "PTLp_med", "TEa_rostral", "ORBm", "PL", "ORBvl", "ILA", "ORBl"]
+    else:
+        labels = ["VISp_caudal", "VISp_caudomed", "VISp_rostromed", "VISal",
+                  "VISam", "AUDp_interm", "AUDd", "MOs_medial", "SSp_tr",
+                  "MOs_caudal", "MOs_lateral", "SSp_bfd", "MOp-bfd",
+                  "MOp-oro", "MOs_rostral", "MOp-tr", "SSp_ll", "SSp_m",
+                  "SSp_n", "SSp_ul", "SSs"]
+
+    # data = np.array(list(roi_dict.values()))
+
+    data = np.array([roi_dict[key] for key in labels])
+    data_cum = data.cumsum(axis=1)
+
+    fig, ax = plt.subplots(figsize=(7.5, 10))
+    ax.invert_yaxis()
+    # ax.xaxis.set_visible(False)
+    # ax.xaxis.
+    ax.set_xlim(0, np.sum(data, axis=1).max())
+
+    for i, (colname, color) in enumerate(zip(names, colors)):
+        widths = data[:, i]
+        starts = data_cum[:, i] - widths
+        ax.barh(labels, widths, left=starts, height=0.8, label=colname, color=color)
+
+    ax.legend(ncol=len(names), bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left', mode="expand", fontsize='large')
+    ax.set_xticks([0, 25, 50, 75, 100])
+    ax.set_xticklabels([0, 25, 50, 75, 100])
+    plt.yticks(fontsize=15)
+    plt.tight_layout()
+    plt.margins(0.01)
+    plt.savefig(out_path, dpi=600)
+    plt.close()
+
+
+def get_div_proportional_values(div_lvl_dict):
+    div_values = np.zeros(4)
+
+    for site in div_lvl_dict:
+        if 'SC.m' in site:
+            div_values[0] += div_lvl_dict[site]
+        elif 'SC.cm' in site:
+            div_values[1] += div_lvl_dict[site]
+        elif 'SC.cl' in site:
+            div_values[2] += div_lvl_dict[site]
+        else:
+            div_values[3] += div_lvl_dict[site]
+
+    div_values = (div_values / np.sum(div_values)) * 100
+
+    # div_values = np.split(div_values, 4)
+    # col_totals = np.sum(div_values, axis=0)
+    # div_values = np.nan_to_num(div_values / col_totals) * 100
+
+    return div_values
+
+
+# Generates 4 stacked bar charts according to ARA level, raw pixel or rational
+def generate_levels_chart(div_lvl_dict, title, out_path, value_type, colors):
+    lvls = 4  # indentation
+    ind = np.arange(lvls)  # the locations for the groups
+    width = 0.35  # the width of the bars
+    div_lists = [[], [], [], []]
+
+    # Place site in appropriate div list
+    for site in div_lvl_dict:
+        if 'SC.m' in site:
+            div_lists[0].append((site, div_lvl_dict[site]))
+        elif 'SC.cm' in site:
+            div_lists[1].append((site, div_lvl_dict[site]))
+        elif 'SC.cl' in site:
+            div_lists[2].append((site, div_lvl_dict[site]))
+        else:
+            div_lists[3].append((site, div_lvl_dict[site]))
+
+    # Sort div lists by ARA level, 86, 90, 96, 100, for SC charts
+    div_lists[0] = [tup[1] for tup in sorted(
+        div_lists[0], key=lambda x: int(x[0].split('_')[1]))]
+    div_lists[1] = [tup[1] for tup in sorted(
+        div_lists[1], key=lambda x: int(x[0].split('_')[1]))]
+    div_lists[2] = [tup[1] for tup in sorted(
+        div_lists[2], key=lambda x: int(x[0].split('_')[1]))]
+    div_lists[3] = [tup[1] for tup in sorted(
+        div_lists[3], key=lambda x: int(x[0].split('_')[1]))]
+
+    # Proportional stacked bar graph values
+    if value_type == 'proportion':
+        plt.ylabel('Proportion of Labeling')
+        col_totals = np.sum(div_lists, axis=0)
+        div_lists = np.nan_to_num(np.array(div_lists) / col_totals) * 100
+
+    else:
+        plt.ylabel('Absolute Value of Pixels')
+
+    bars = []
+    bottoms = np.zeros(4)
+    for idx, division in enumerate(div_lists):
+        bars.append(plt.bar(
+            ind, division, bottom=bottoms, color=colors[idx], width=width))
+        bottoms += division
+
+    plt.title(title)
+    plt.xticks(ind, ('ARA 86', 'ARA 90', 'ARA 96', 'ARA 100'))
+    plt.legend((bars[0][0], bars[1][0], bars[2][0], bars[3][0]),
+               ('SC.m', 'SC.cm', 'SC.cl', 'SC.l'))
+
+    plt.savefig(out_path, dpi=600)
+    plt.close()
+
+
+def plot_stacked(div_lists, colors, bar_num, out_path):
+    lvls = bar_num  # number of bars
+    ind = np.arange(lvls)  # the locations for the groups
+    width = 0.35  # the width of the bars
+
+    bars = []
+    bottoms = np.zeros(bar_num)
+    for idx, division in enumerate(div_lists):
+        bars.append(plt.bar(
+            ind, division, bottom=bottoms, color=colors[idx], width=width))
+        bottoms += division
+
+    plt.savefig(out_path, dpi=600)
+    plt.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/plot_table.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import matplotlib
+# matplotlib.use('Agg')
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import os
+from cic_dis import cic_utils
+import cPickle as pickle
+
+# Replacement names for default ROI names
+ROI_NAMES = {
+    'SCzo_div1': 'SC.m-zo',
+    'SCsg_div1': 'SC.m-sg',
+    'SCop_div1': 'SC.m-op',
+    'SCig_div1': 'SC.m-ig',
+    'SCiw_div1': 'SC.m-iw',
+    'SCdg_div1': 'SC.m-dg',
+    'SCdw_div1': 'SC.m-dw',
+    'SCzo_div2': 'SC.cm-zo',
+    'SCsg_div2': 'SC.cm-sg',
+    'SCop_div2': 'SC.cm-op',
+    'SCig_div2': 'SC.cm-ig',
+    'SCiw_div2': 'SC.cm-iw',
+    'SCdg_div2': 'SC.cm-dg',
+    'SCdw_div2': 'SC.cm-dw',
+    'SCzo_div3': 'SC.cl-zo',
+    'SCsg_div3': 'SC.cl-sg',
+    'SCop_div3': 'SC.cl-op',
+    'SCig_div3': 'SC.cl-ig',
+    'SCiw_div3': 'SC.cl-iw',
+    'SCdg_div3': 'SC.cl-dg',
+    'SCdw_div3': 'SC.cl-dw',
+    'SCig_div4': 'SC.l-ig',
+    'SCiw_div4': 'SC.l-iw',
+    'SCdg_div4': 'SC.l-dg',
+    'SCdw_div4': 'SC.l-dw'
+}
+
+# Custom COL order provided by Nora for columns
+COL_ORDER = ['SC.m-zo', 'SC.m-sg', 'SC.m-op', 'SC.m-ig', 'SC.m-iw', 'SC.m-dg',
+             'SC.m-dw', 'SC.cm-zo', 'SC.cm-sg', 'SC.cm-op', 'SC.cm-ig',
+             'SC.cm-iw', 'SC.cm-dg', 'SC.cm-dw', 'SC.cl-zo', 'SC.cl-sg',
+             'SC.cl-op', 'SC.cl-ig', 'SC.cl-iw', 'SC.cl-dg', 'SC.cl-dw',
+             'SC.l-ig', 'SC.l-iw', 'SC.l-dg', 'SC.l-dw']
+
+# Custom ROW order provided by Nora for group SC-assoc-stacked_bar_ant
+ROW_ORDER_1 = ["RSPv_interm", "RSPv_caudal", "RSPd_rostral", "RSPagl",
+               "PTLp_lat", "RSPd_caudal", "ACAv_interm", "TEa_caudal",
+               "ACAv_caudal", "PTLp_med", "ACAd_interm", "ACAv_rostral",
+               "RSPv_rostral", "ACAd_rostral", "ACAd_caudal", "ORBm", "PL",
+               "ILA", "TEa_rostral", "ORBvl", "ORBl"]
+
+# Custom ROW order provided by Nora for group SC SC-sensory-stacked-bar-ant
+ROW_ORDER_2 = ["VISp_caudal", "VISp_caudomed", "VISp_rostromed", "VISal",
+               "VISam", "AUDp_interm", "AUDd", "MOs_medial", "SSp_tr",
+               "MOs_caudal", "MOs_lateral", "SSp_bfd", "MOp-bfd", "MOp-oro",
+               "MOs_rostral", "MOp-tr", "SSp_ll", "SSp_m", "SSp_n", "SSp_ul",
+               "SSs"]
+
+# COLORS used for each subsection
+COLORS = ('#990000', '#FF6600', '#116600', '#660078')
+NAMES = ('SC.m', 'SC.cm', 'SC.cl', 'SC.l')
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Creates matrix table from connectivity matrix, "
+                    "no communities")
+
+    parser.add_argument('-m', '--matrix_csv',
+                        help='Row/column based connectivity matrix',
+                        required=True)
+
+    parser.add_argument('-v', '--verbose',
+                        help='Print matrix, cluster related output',
+                        action='store_true')
+
+    parser.add_argument('-fmt', '--format',
+                        help='Format of plot (e.g. svg, png,...)',
+                        default='svg')
+
+    parser.add_argument('-fs', '--fontsize',
+                        help='Font size of tick labels in x and y',
+                        type=int,
+                        default=6)
+
+    parser.add_argument('-rn', '--roi_replacement',
+                        help='Replaces default roi names with ROI_NAMES',
+                        action='store_true')
+
+    parser.add_argument('-rc', '--reorder_column',
+                        help='Replaces default roi names with ROI_NAMES',
+                        action='store_true')
+
+    parser.add_argument('-rr', '--reorder_row',
+                        help='Replaces default roi names with ROI_NAMES',
+                        action='store_true')
+
+    parser.add_argument('-ns', '--no_shading',
+                        help='Table with no shading, only values ',
+                        action='store_true')
+
+    parser.add_argument('-p', '--percentage',
+                        help='Creates table with percentage values',
+                        action='store_true')
+
+    parser.add_argument('-l', '--legend',
+                        help='Adds colorbar next to figure',
+                        action='store_true')
+
+    args = vars(parser.parse_args())
+
+    matrix_csv_path = args['matrix_csv']
+    assert os.path.isfile(matrix_csv_path),\
+        "Can't find row column matrix csv file {}".format(matrix_csv_path)
+
+    # PARSE ARGUMENTS
+    verbose = args['verbose']
+    fontsize = args['fontsize']
+    roi_replacement = args['roi_replacement']
+    reorder_column = args['reorder_column']
+    reorder_row = args['reorder_row']
+    no_shading = args['no_shading']
+    percentage = args['percentage']
+    legend = args['legend']
+    fmt = "{}".format(args['format'])
+    output_dir, base_name = os.path.split(matrix_csv_path)
+    chart_type = "_table"
+
+    if no_shading:
+        chart_type += "_no_shading"
+    if percentage:
+        chart_type += "_percentage"
+
+    graph_name = base_name.replace(
+        '.csv',
+        '{}.{}'.format(chart_type, fmt))
+
+    out_path = os.path.join(output_dir, graph_name)
+
+    # Open, read input matrix csv
+    (row_roi_name_npa, col_roi_name_npa, ctx_mat_npa) = \
+        cic_utils.read_ctx_mat(matrix_csv_path)
+
+    if roi_replacement:
+        # Replace roi names with custom ROI_NAMES
+        col_roi_name_npa = np.array(
+            [ROI_NAMES[roi] for roi in col_roi_name_npa])
+
+    if verbose:
+        print("Read row ROIs: \n{}\n".format(row_roi_name_npa))
+        print("Read col ROIs: \n{}\n".format(col_roi_name_npa))
+
+    # Create in case no changes are needed
+    new_col_roi_indices = np.arange(len(col_roi_name_npa))
+    new_row_roi_indices = np.arange(len(row_roi_name_npa))
+
+    # Reordering columns
+    if reorder_column:
+        new_col_roi_indices = []
+        col_order_dict = dict(
+            [(roi, indx) for indx, roi in enumerate(col_roi_name_npa)])
+
+        for roi in COL_ORDER:
+            if roi in col_roi_name_npa:
+                new_col_roi_indices.append(col_order_dict[roi])
+
+    # Reordering rows
+    if reorder_row:
+        new_row_roi_indices = []
+        row_order_dict = dict(
+            [(roi, indx) for indx, roi in enumerate(row_roi_name_npa)])
+
+        # Determine what row order to use
+        if 'assoc' in matrix_csv_path:
+            row_order = ROW_ORDER_1
+        else:
+            row_order = ROW_ORDER_2
+
+        for roi in row_order:
+            if roi in row_roi_name_npa:
+                new_row_roi_indices.append(row_order_dict[roi])
+
+    # Reorder ctx matrix npa
+    new_col_roi_name_npa = np.array(col_roi_name_npa[new_col_roi_indices])
+    new_row_roi_name_npa = np.array(row_roi_name_npa[new_row_roi_indices])
+    temp_rs = ctx_mat_npa[new_row_roi_indices, :]
+    reorder_ctx_mat_npa = temp_rs[:, new_col_roi_indices]
+
+    if percentage:
+        for i in range(len(new_row_roi_name_npa)):
+            row_total = np.sum(reorder_ctx_mat_npa[i])
+            reorder_ctx_mat_npa[i] = reorder_ctx_mat_npa[i] / row_total
+
+    # Plot creation
+    fig, ax = plt.subplots(figsize=(7.5, 10))
+
+    im = heatmap(ax=ax,
+                 ctx_mat_npa=reorder_ctx_mat_npa,
+                 row_roi_name_npa=new_row_roi_name_npa,
+                 col_roi_name_npa=new_col_roi_name_npa,
+                 no_shading=no_shading,
+                 fontsize=fontsize,
+                 legend=legend)
+
+    annotate_heatmap(im=im,
+                     ctx_mat_npa=reorder_ctx_mat_npa,
+                     row_roi_name_npa=new_row_roi_name_npa,
+                     col_roi_name_npa=new_col_roi_name_npa,
+                     no_shading=no_shading)
+
+    fig.tight_layout()
+    plt.savefig(out_path, dpi=600)
+    pickle_path = cic_utils.pickle_path(out_path)
+    pickle.dump(args, open(pickle_path, "wb"))
+
+
+def annotate_heatmap(im, ctx_mat_npa, row_roi_name_npa,
+                     col_roi_name_npa, no_shading):
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(row_roi_name_npa)):
+        for j in range(len(col_roi_name_npa)):
+            value = ctx_mat_npa[i, j]
+            color = 'black' if value < 0.6 or no_shading else 'w'
+            im.axes.text(j, i, round(value, 4),
+                         ha="center", va="center", color=color,
+                         rotation=90, fontsize=6)
+
+
+def heatmap(ax, ctx_mat_npa, row_roi_name_npa,
+            col_roi_name_npa, no_shading, fontsize, legend):
+
+    if no_shading:
+        reorder_ctx_mat_npa_zeros = np.zeros(
+            (len(row_roi_name_npa), len(col_roi_name_npa)))
+        im = ax.imshow(reorder_ctx_mat_npa_zeros,
+                       cmap=plt.cm.gray_r,
+                       aspect='auto',
+                       vmin=0,
+                       vmax=1)
+    else:
+        im = ax.imshow(ctx_mat_npa,
+                       cmap=plt.cm.gray_r,
+                       aspect='auto',
+                       vmin=0,
+                       vmax=1)
+
+    # Create colorbar
+    if legend:
+        cbar = ax.figure.colorbar(im, ax=ax,)
+        # cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+        cbar.ax.set_ylabel("", rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(len(col_roi_name_npa)))
+    ax.set_yticks(np.arange(len(row_roi_name_npa)))
+    # ... and label them with the respective list entries
+    ax.set_xticklabels(col_roi_name_npa, fontsize=fontsize)
+    ax.set_yticklabels(row_roi_name_npa, fontsize=fontsize)
+
+    ax.grid(which='minor', color='black', linestyle='-', linewidth=0.5)
+
+    ax.set_xticks(np.arange(ctx_mat_npa.shape[1] + 1) - .5,
+                  minor=True)
+    ax.set_yticks(np.arange(ctx_mat_npa.shape[0] + 1) - .5,
+                  minor=True)
+
+    ax.tick_params(which="minor", bottom=False, left=False)
+    ax.tick_params(top=True, bottom=False,
+                   labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=90,)
+
+    return im
+
+
+def reorder_columns_rows():
+    pass
+
+
+if __name__ == '__main__':
+    main()
